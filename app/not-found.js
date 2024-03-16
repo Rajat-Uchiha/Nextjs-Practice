905fd38d939a39e50956a2b5758845fcf75bdd25
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-03-16 13:41:46 +0530

    404 page

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center font-bold text-7xl bg-black text-white">
+      404! PAGE NOT FOUND
+    </div>
+  );
+};
+
+export default notFound;
